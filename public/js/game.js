830d18a4adb9819b1fb1358354e65c966cb91437
Meghana Putta <meghanareddy.putta@gmail.com>
2018-10-28 18:20:01 -0500

    commit

--- public/js/game.js
+++ public/js/game.js
@@ -2,18 +2,18 @@ function ageCalculate() {
     var bDate = document.getElementById('birth_date').value;
 
     var d = new Date(bDate);
-    var mDate = bDate.toString();
+    var d1 = bDate.toString();
 
-    var y = parseInt(mdate.substring(0, 4), 10);
+    var y = parseInt(d1.substring(0, 4), 10);
 
-    var m = parseInt(mdate.substring(5, 7), 10);
+    var m = parseInt(d1.substring(5, 7), 10);
 
-    var d = parseInt(mdate.substring(8, 10), 10);
+    var d = parseInt(d1.substring(8, 10), 10);
     var now = new Date();
 
     var bday = new Date(y, m - 1, d);
-    var differenceInMilisecond = now.valueOf() - bday.valueOf();
-    var newage = Math.floor(differenceInMilisecond / 31536000000);
+    var diff = now.valueOf() - bday.valueOf();
+    var newage = Math.floor(diff / 31536000000);
 
     document.getElementById("age").innerHTML = newage + " years ";
     if ((today.getMonth() == bday.getMonth()) && (now.getDate() == bday.getDate())) {
